4441d966deea60d237b4f10788ef29705c79fb01
RishavK010 <rishavkamal@gmail.comclear>
2025-03-14 03:12:45 +0530

    updated index.js

--- Zerodha-Clone/backend/index.js
+++ Zerodha-Clone/backend/index.js
@@ -13,7 +13,12 @@ const { OrdersModel } = require("./model/OrdersModel");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",  
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(bodyParser.json());
 
 const connectDB = async () => {
